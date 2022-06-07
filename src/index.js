d019cfabd95d683a6c2d6b799efc9a5035885c04
milad.xsar23 <milad.xsar72@gmail.com>
2022-06-07 11:49:18 +0430

    add project  scafloading

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import httpLink from './api';
+import './index.scss';
+import App from './components/App/App';
+import reportWebVitals from './reportWebVitals';
+
+/*configuration cache for apollo client*/
+const cache = new InMemoryCache()
+/*configuration apollo client*/
+const client = new ApolloClient({
+  link : httpLink,
+  cache
+})
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
